ded71a6a721ea2abe3c4d0c5cdc0dca51a70f418
boxak <boxak@naver.com>
2022-01-25 23:23:27 +0900

    2021-01-25 UserDaoTest AssertThat을 통한 검증

--- src/main/java/dao/UserDaoTest.java
+++ src/main/java/dao/UserDaoTest.java
@@ -1,36 +1,41 @@
 package dao;
 
 import domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
-        ApplicationContext context =
-                new GenericXmlApplicationContext("applicationContext.xml");
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        Assertions.assertThat(dao.getCount())
+                .isEqualTo(0);
+
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
+        Assertions.assertThat(dao.getCount())
+                .isEqualTo(1);
 
         User user2 = dao.get(user.getId());
 
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
+        Assertions.assertThat(user2.getName())
+                .isNotEmpty()
+                .isEqualTo(user.getName());
+        Assertions.assertThat(user2.getPassword())
+                .isNotEmpty()
+                .isEqualTo(user.getPassword());
     }
 }

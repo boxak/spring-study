f21d2747fe145115186897f3ad1d5588afebc8dd
boxak <boxak@naver.com>
2022-01-28 15:25:10 +0900

    2021-01-28 while문 마다 계산 콜백을 불러와서 공통으로 등장하는 로직을 최소화. 정리하자면, 템플릿은 콜백을 불러와서 사용하는 클라이언트이자 컨텍스트 코드. 달라지는 부분만 콜백 인터페이스를 상속받고 템플릿에서 내부 클래스로 구현하도록 수정.

--- src/main/java/template/Calculator.java
+++ src/main/java/template/Calculator.java
@@ -1,6 +1,7 @@
 package template;
 
 import template.callback.BufferedReaderCallback;
+import template.callback.LineCallback;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,34 +9,25 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        return fileReadTemplate(filepath, new BufferedReaderCallback() {
+        LineCallback sumCallback = new LineCallback() {
             @Override
-            public Integer workWithReader(BufferedReader br) throws IOException {
-                String line = null;
-                Integer sum = 0;
-                while((line = br.readLine()) != null) {
-                    sum+=Integer.parseInt(line);
-                }
-
-                return sum;
+            public Integer workWithLine(String line, Integer value) {
+                return value+=Integer.parseInt(line);
             }
-        });
+        };
+
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        return fileReadTemplate(filepath, new BufferedReaderCallback() {
+        LineCallback multiplyCallback = new LineCallback() {
             @Override
-            public Integer workWithReader(BufferedReader br) throws IOException {
-                String line = null;
-                Integer product = 1;
-
-                while((line = br.readLine()) != null) {
-                    product*=Integer.parseInt(line);
-                }
-
-                return product;
+            public Integer workWithLine(String line, Integer value) {
+                return value*=Integer.parseInt(line);
             }
-        });
+        };
+
+        return lineReadTemplate(filepath, multiplyCallback, 1);
     }
 
     public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback)
@@ -45,6 +37,32 @@ public class Calculator {
             br = new BufferedReader(new FileReader(filepath));
             int ret = callback.workWithReader(br);
             return ret;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal)
+            throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer res = initVal;
+            String line = null;
+            while((line = br.readLine()) != null) {
+                res = callback.workWithLine(line, res);
+            }
+
+            return res;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
---
+++ src/main/java/template/callback/LineCallback.java
@@ -0,0 +1,5 @@
+package template.callback;
+
+public interface LineCallback {
+    Integer workWithLine(String line, Integer value);
+}

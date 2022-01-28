f2607e920b26c2ce7ca37701ef6a26d97202bb5e
boxak <boxak@naver.com>
2022-01-28 22:45:24 +0900

    2021-01-28 List로 여러 row를 가져오는 경우, jdbdTemplate.query 구문을 사용하면 된다.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
     private DataSource dataSource;
@@ -26,7 +27,7 @@ public class UserDao {
     public void add(User user) throws SQLException {
         this.jdbcTemplate.update(
                 "insert into users (id, name, password)" +
-                        "values(?, ?, ?)",
+                        " values(?, ?, ?)",
                 user.getId(),
                 user.getName(),
                 user.getPassword()
@@ -34,19 +35,31 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException {
-        return this.jdbcTemplate.queryForObject("select * from users where id=?",
-                new Object[]{id},
-                new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
+        return this.jdbcTemplate.queryForObject("select * from users where id=?", new Object[]{id}, new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
 
-                        return user;
-                    }
-                });
+                return user;
+            }
+        });
+    }
+
+    public List<User> getAll() throws SQLException {
+        return this.jdbcTemplate.query("select * from users order by id", new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+
+                return user;
+            }
+        });
     }
 
     public void deleteAll() throws SQLException {
--- src/main/java/test/UserDaoTest.java
+++ src/main/java/test/UserDaoTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/test-applicationContext.xml")
@@ -67,5 +68,39 @@ public class UserDaoTest {
         Assertions.assertThat(dao.getCount()).isEqualTo(0);
 
         dao.get("unknown_id");
+    }
+
+    @Test
+    public void getAllTest() throws SQLException {
+        dao.deleteAll();
+
+        User user1 = new User("gyumee","박성철","springno1");
+        User user2 = new User("leegw700","이길원","springno2");
+        User user3 = new User("bumjin","박범진","springno3");
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        Assertions.assertThat(users1.size()).isEqualTo(1);
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        Assertions.assertThat(users2.size()).isEqualTo(2);
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        Assertions.assertThat(users3.size()).isEqualTo(3);
+        checkSameUser(user1, users3.get(1));
+        checkSameUser(user2, users3.get(2));
+        checkSameUser(user3, users3.get(0));
+    }
+
+    private void checkSameUser(User user1, User user2) {
+        Assertions.assertThat(user1.getId()).isEqualTo(user2.getId());
+        Assertions.assertThat(user1.getName()).isEqualTo(user2.getName());
+        Assertions.assertThat(user1.getPassword()).isEqualTo(user2.getPassword());
     }
 }

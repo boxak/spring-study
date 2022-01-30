252b5bee98b78d5f4b55099bc8540eb5eb2f45c4
boxak <boxak@naver.com>
2022-01-30 21:26:03 +0900

    2022-01-30 sql 구문의 오류 수정.

--- src/main/java/dao/UserDaoJdbc.java
+++ src/main/java/dao/UserDaoJdbc.java
@@ -71,13 +71,14 @@ public class UserDaoJdbc implements UserDao {
     @Override
     public void update(User user) {
         this.jdbcTemplate.update(
-                "update users set name=?, password=?, level=?, login=?, recommend=?" +
-                        "",
+                "update users set name=?, password=?, level=?, login=?, recommend=? " +
+                        "where id=?",
                 user.getName(),
                 user.getPassword(),
                 user.getLevel().intValue(),
                 user.getLogin(),
-                user.getRecommend()
+                user.getRecommend(),
+                user.getId()
         );
     }
 }

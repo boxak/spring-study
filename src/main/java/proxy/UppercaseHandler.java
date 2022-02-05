1c654d38aa042a56b8c76ebbac985d1c2188328c
boxak <boxak@naver.com>
2022-02-05 18:15:30 +0900

    2022-02-05 invoke 메서드에 메서드 이름을 조건으로 실행되게 할 수 있다.

--- src/main/java/proxy/UppercaseHandler.java
+++ src/main/java/proxy/UppercaseHandler.java
@@ -15,7 +15,7 @@ public class UppercaseHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object ret = method.invoke(target, args);
 
-        if (ret instanceof String) {
+        if (ret instanceof String && method.getName().startsWith("say")) {
             return ((String)ret).toUpperCase();
         } else return ret;
 

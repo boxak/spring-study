2db70b7b7c099d1666f06858294d67f4e57d0a9a
boxak <boxak@naver.com>
2022-02-10 22:10:08 +0900

    2022-02-10 트랜잭션 경계설정의 일원화를 위해 DAO에 접근할 때는 항상 비즈니스 로직이 담긴 서비스 계층을 거치도록 하는 것이 바람직하다.

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,7 +2,12 @@ package service;
 
 import domain.User;
 
+import java.util.List;
+
 public interface UserService {
     void add(User user);
+    List<User> getAll();
+    void deleteAll();
+    void update(User user);
     void upgradeLevels();
 }
--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -56,6 +56,21 @@ public class UserServiceImpl implements UserService {
         userDao.add(user);
     }
 
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+
+    @Override
+    public void deleteAll() {
+        userDao.deleteAll();
+    }
+
+    @Override
+    public void update(User user) {
+        userDao.update(user);
+    }
+
     private void sendUpgradeEMail(User user) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(user.getEmail());

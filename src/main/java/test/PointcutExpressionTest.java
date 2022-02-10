279c179bdc903649b9f5e2e48647f4b90fcddbf7
boxak <boxak@naver.com>
2022-02-10 14:23:11 +0900

    2022-02-10 Aspectj의 pointcut expression 테스트. AspectjExpressionPointcut 객체 생성에 Aspectjweaver 라이브러리 필요.

--- src/main/java/test/PointcutExpressionTest.java
+++ src/main/java/test/PointcutExpressionTest.java
@@ -1,14 +1,31 @@
 package test;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import pointcut.Bean;
+import pointcut.Target;
 
 public class PointcutExpressionTest {
 
     @Test
     public void methodSignaturePointcut() throws SecurityException, NoSuchMethodException {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
-        pointcut.setExpression("");
+        pointcut.setExpression("execution(public int " +
+                "pointcut.Target.minus(int,int) " +
+                "throws java.lang.RuntimeException)");
+
+        Assertions.assertThat(pointcut.getClassFilter().matches(Target.class) &&
+                pointcut.getMethodMatcher().matches(Target.class.getMethod("minus", int.class, int.class),null)).isTrue();
+
+        Assertions.assertThat(pointcut.getClassFilter().matches(Target.class) &&
+                pointcut.getMethodMatcher().matches(Target.class.getMethod("plus", int.class, int.class), null))
+                .isFalse();
+
+        Assertions.assertThat(pointcut.getClassFilter().matches(Bean.class) &&
+                pointcut.getMethodMatcher().matches(
+                        Target.class.getMethod("method"),null
+                )).isFalse();
     }
 
 }

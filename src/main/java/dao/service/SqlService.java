99c821ffd433511bb0d45666e9f0548362b7b7d5
boxak <boxak@naver.com>
2022-04-12 12:11:13 +0900

    2022-04-11 jaxb로 java 오브젝트 생성

--- src/main/java/dao/service/SqlService.java
+++ src/main/java/dao/service/SqlService.java
@@ -1,7 +1,5 @@
 package dao.service;
 
-import java.sql.SQLException;
-
 public interface SqlService {
     String getSql(String key) throws SqlRetrievalFailureException;
 }
---
+++ src/main/java/dao/service/jaxb/package-info.java
@@ -0,0 +1,9 @@
+//
+// �� ������ JAXB(JavaTM Architecture for XML Binding) ���� ���� 2.2.8-b130911.1802 ������ ���� �����Ǿ����ϴ�. 
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>�� �����Ͻʽÿ�. 
+// �� ������ �����ϸ� �ҽ� ��Ű���� ���������� �� ���� ������ �սǵ˴ϴ�. 
+// ���� ��¥: 2022.04.12 �ð� 12:03:02 PM KST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.epril.com/sqlmap", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package dao.service.jaxb;

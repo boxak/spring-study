70254611afb401218bf61db46b996eb13011bc1c
boxak <boxak@naver.com>
2022-04-13 17:34:35 +0900

    2022-04-13 sqlService 안의 sqlReader, sqlRegistry 를 다른 빈으로 분리해서 등록.

---
+++ src/main/java/dao/service/JaxbXmlSqlReader.java
@@ -0,0 +1,38 @@
+package dao.service;
+
+import dao.service.jaxb.SqlType;
+import dao.service.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
+public class JaxbXmlSqlReader implements SqlReader {
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
+
+        String contextPath = Sqlmap.class.getPackage().getName();
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            InputStream inputStream = getClass().getResourceAsStream(sqlmapFile);
+
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(inputStream);
+
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

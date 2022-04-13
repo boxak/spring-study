209019121ec70f8f8bd33dc78325b39aa292442d
boxak <boxak@naver.com>
2022-04-13 17:42:55 +0900

    2022-04-13 sqlService 안의 sqlReader, sqlRegistry 를 default 의존관계를 갖는 빈으로 등록해서 사용.

---
+++ src/main/java/dao/service/DefaultSqlService.java
@@ -0,0 +1,8 @@
+package dao.service;
+
+public class DefaultSqlService extends BaseSqlService {
+    public DefaultSqlService() {
+        setSqlReader(new JaxbXmlSqlReader());
+        setSqlRegistry(new HashMapSqlRegistry());
+    }
+}
